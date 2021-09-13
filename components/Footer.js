e1ecf8913f8e1adb4a56fd3c8710f97bbb31f04b
Awonkhrais <awonkhrais@gmail.com>
2021-09-13 20:45:19 +0300

    solve lab37

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+return(
+
+<footer className="p-4 bg-green-500 text-xl">
+    &copy; 2021
+</footer>
+
+)
+
+}
---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+
+    return(
+        <header className="p-4 bg-green-500 text-4xl">
+        <h1>Cookie Stand Admin</h1>
+      </header>
+    )
+} 
